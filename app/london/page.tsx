9a99667085b935bb8bf9e60de3c1fab154a5acd5
Simrun Basuita <hello@sim.run>
2024-07-31 15:19:13 +0100

    header nav bar with about button

--- app/london/page.tsx
+++ app/london/page.tsx
@@ -4,7 +4,8 @@ import type { EventInput } from "@fullcalendar/core";
 import dayGrid from "@fullcalendar/daygrid";
 import iCalendarPlugin from "@fullcalendar/icalendar";
 import FullCalendar from "@fullcalendar/react";
-import Link from "next/link";
+
+import { useRouter } from "next/navigation";
 
 const eventSources = [
   {
@@ -34,8 +35,15 @@ const eventSources = [
 ];
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <div className="flex h-svh flex-col">
+      <div className="flex justify-between text-2xl">
+        <p>CI Calendar UK</p>
+        <p>London</p>
+      </div>
+
       <div className="flex-1">
         <FullCalendar
           plugins={[dayGrid, iCalendarPlugin]}
@@ -44,20 +52,17 @@ export default function Page() {
           eventDisplay="block"
           displayEventTime={false}
           height="100%"
+          headerToolbar={{ start: "title", end: "about today prev,next" }}
+          customButtons={{
+            about: { text: "about", click: () => router.push("/about") },
+          }}
+          titleFormat={{ year: "2-digit", month: "short" }}
           eventClick={(info) => {
             // don't navigate away from calendar; open event urls in new window
             info.jsEvent.preventDefault();
             if (info.event.url) window.open(info.event.url, "_blank");
           }}
         />
-      </div>
-
-      <div className="grid grid-cols-3">
-        <p className="text-start">CI Calendar UK</p>
-        <p className="text-center">London</p>
-        <Link href="/about" className="text-end">
-          About
-        </Link>
       </div>
     </div>
   );
