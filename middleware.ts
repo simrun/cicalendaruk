9027848752ad836895654dcde870c4ce56f5bd83
Simrun Basuita <hello@sim.run>
2024-07-31 09:56:50 +0100

    delete comment

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { MiddlewareConfig, NextMiddleware } from "next/dist/server/web/types";
 
-// Proxy ICS requests as they're cross origin
-
 const icsPathToEnvVar: Map<string, string> = new Map([
   ["/feeds/ricknodine.ics", "ICS_URL_RICKNODINE"],
   ["/feeds/cigoldsmiths.ics", "ICS_URL_CIGOLDSMITHS"],
