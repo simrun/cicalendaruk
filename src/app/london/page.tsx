a3bb7474daeb7c5b909a6a1a3413fe5c64857f13
John Mellor <mellor@gmail.com>
2024-08-04 01:22:27 +0100

    Parse event urls from description field of Google Calendar feeds

--- src/app/london/page.tsx
+++ src/app/london/page.tsx
@@ -9,12 +9,54 @@ import { useRouter } from "next/navigation";
 
 import NavBar from "@/components/NavBar";
 
-// Google Calendar doesn't have a URL field
-// Fill it from the Location field instead
-function eventUrlFromLocation(eventData: EventInput) {
-  if (eventData.extendedProps?.location) {
-    eventData.url = eventData.extendedProps.location;
+// Google Calendar doesn't have a URL field, so infer URLs from the location
+// or description fields if necessary.
+function addEventUrl(eventData: EventInput) {
+  // If event already has a URL, leave it as is.
+  if (eventData.url) {
+    return eventData;
   }
+
+  // Next check if the location field is an absolute URL.
+  try {
+    eventData.url = new URL(eventData.extendedProps?.location).toString();
+    return eventData;
+  } catch (e) {
+    // Location is blank or not an absolute URL.
+  }
+
+  // Finally try to use the first URL found in the description field.
+  let description = (eventData.extendedProps?.description as string) ?? "";
+  // The description field set by Google Calendar is sometimes plain text, like:
+  //     'Prev line\nhttps://domain.com/path?a=b&c=d#hash\nNext line'
+  // and sometimes HTML, like:
+  //     'Prev line<br><a href="https://domain.com/path?a=b&amp;c=d#hash">https://domain.com/path?a=b&amp;c=d#hash</a><br>Next line'
+  // We have to detect which in order to know how to decode it!
+  if (/<br>|<\/a>/.test(description)) {
+    // Strip HTML tags, replacing them with whitespace rather than nothing since
+    // tags like <br> and <p> often act as spacing.
+    description = description.replace(/(<([^>]+)>)/gi, " ");
+    // Decode HTML entities.
+    const txt = document.createElement("textarea");
+    txt.innerHTML = description;
+    description = txt.value;
+  }
+  // Search for a URL that starts with http(s):// or www., contains no spaces or
+  // quotes, and ends with a unicode word character, closing parenthesis, or
+  // slash. Also, hostnames must contain a dot or (IPv6) colon.
+  const urlRegex = /\b(?:https?:\/\/[^'"\s.]+[.:]|www\.)[^'"\s]+[\w)\/]/u;
+  const urlMatch = description.match(urlRegex);
+  if (urlMatch) {
+    let url = urlMatch[0];
+    // Remove unbalanced trailing closing parentheses.
+    let excessParens =
+      (url.match(/\)/g) ?? []).length - (url.match(/\(/g) ?? []).length;
+    while (url.at(-1) === ")" && excessParens-- > 0) {
+      url = url.slice(0, -1);
+    }
+    eventData.url = url;
+  }
+
   return eventData;
 }
 
@@ -34,13 +76,11 @@ const eventSources = [
     url: "/feeds/misc.ics",
     format: "ics",
     color: "green",
-    eventDataTransform: eventUrlFromLocation,
   },
   {
     url: "/feeds/uk.ics",
     format: "ics",
     color: "red",
-    eventDataTransform: eventUrlFromLocation,
   },
 ];
 
@@ -57,6 +97,7 @@ export default function Page() {
           initialView="dayGridMonth"
           firstDay={1}
           eventSources={eventSources}
+          eventDataTransform={addEventUrl}
           eventDisplay="block"
           displayEventTime={false}
           height="100%"
