bfcb4e6dfd64ba5a4c430c6647f4404aba6a4d1c
Simrun Basuita <hello@sim.run>
2024-08-30 17:32:34 +0100

    re-order code

--- src/components/Expandable.tsx
+++ src/components/Expandable.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from "react";
 
-function Summary({ children }: { children: ReactNode }) {
-  return <summary className="cursor-pointer font-bold">{children}</summary>;
-}
-
 function Expandable({ children }: { children: ReactNode }) {
   return (
     <details className="group rounded border border-gray-400 p-3">
       {children}
     </details>
   );
+}
+
+function Summary({ children }: { children: ReactNode }) {
+  return <summary className="cursor-pointer font-bold">{children}</summary>;
 }
 
 Expandable.Summary = Summary;
