704451d751de5996241af01d9d15d76ae6efd36d
Simrun Basuita <hello@sim.run>
2024-11-20 12:28:23 +0000

    Homepage margins and padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import NavBar from "@/components/NavBar";
 
 export default function Page() {
   return (
-    <div className="m-auto max-w-prose">
+    <div className="m-auto max-w-prose px-2 py-1">
       <NavBar />
+
       <div className="prose">
-        <ul>
+        <p className="mb-0">Select a region:</p>
+
+        <ul className="mt-0">
           <li className="list-['🌉']">
             <Link href={"/bristol"}>Bristol</Link>
           </li>
