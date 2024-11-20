a0dd33bb58a00572861d5647fd7f705165c0f8df
Simrun Basuita <hello@sim.run>
2024-11-20 13:32:36 +0000

    Don't use a footer on the homepage as it gets lost on desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,27 @@ import NavBar from "@/components/NavBar";
 export default function Page() {
   return (
     <MaxWidthContainer>
-      <div className="flex h-svh flex-col">
-        <NavBar />
-        <div className="prose flex-grow">
-          <p>Weekly calendar of contact improvisation events.</p>
+      <NavBar />
+      <div className="prose">
+        <p>Weekly calendar of contact improvisation events.</p>
 
-          <p className="mb-0">Select a region:</p>
-          <ul className="mt-0">
-            <li className="list-['🌉']">
-              <Link href={"/bristol"}>Bristol</Link>
-            </li>
-            <li className="list-['🚇']">
-              <Link href={"/london"}>London</Link>
-            </li>
-          </ul>
-        </div>
+        <p className="mb-0">Select a region:</p>
+        <ul className="mt-0">
+          <li className="list-['🌉']">
+            <Link href={"/bristol"}>Bristol</Link>
+          </li>
+          <li className="list-['🚇']">
+            <Link href={"/london"}>London</Link>
+          </li>
+        </ul>
 
-        <footer className="prose">
-          <p className="mb-2 text-center text-xs">
-            Made with 🤍 by Sim and John. Source code on{" "}
+        <div className="mt-10 text-sm prose-p:m-1">
+          <p>Made with 🤍 by Sim and John.</p>
+          <p>
+            Source code on{" "}
             <Link href="https://github.com/simrun/cicalendaruk">Github</Link>.
           </p>
-        </footer>
+        </div>
       </div>
     </MaxWidthContainer>
   );
